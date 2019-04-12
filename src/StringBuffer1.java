a39159e77982c817021828b5fe57c9731e7cbb10
AmrutTrivedi <amrut.trivedi@revature.com>
2019-04-12 18:26:40 +0530

    Updated Programs

---
+++ src/StringBuffer1.java
@@ -0,0 +1,10 @@
+
+public class StringBuffer1 {
+public static void main(String args[])
+{
+	StringBuffer sb = new StringBuffer("Hello Java");
+	sb.replace(1,3,"First");
+	System.out.println(sb);
+	
+}
+}
